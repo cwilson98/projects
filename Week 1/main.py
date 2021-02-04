1286e917ff51c8953e2b922e383510be52ee8ae7
cwilson98 <5wilsc18@solent.ac.uk>
2021-02-04 07:43:16 -0500

    Complete Question 1

--- Week 1/main.py
+++ Week 1/main.py
@@ -1,8 +1,4 @@
-print("Who sees me rolling (cops, wardens, other)?")
-ans = input()
-if ans == 'cops':
-    print("They see me rolling. They hating. They hoping they gonna catch me riding dirty!.")
-elif ans == 'wardens':
-    print("My music's so loud. I'm swanging. They hoping they gonna catch me riding dirty!")
-else:
-    print("Trying to catch me riding dirty!")
+print("Who broke my heart?")
+name = input()
+print("My first love " + name + " broke my heart for the first time.")
+print("And I was like baby, baby, baby oh.")
