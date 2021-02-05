207d9357319e8475035a9bdf75e54693aa95a332
cwilson98 <78536301+cwilson98@users.noreply.github.com>
2021-02-05 18:44:14 -0500

    Completed Question 1-6

--- Week 1/main.py
+++ Week 1/main.py
@@ -49,6 +49,32 @@ def q5():
             print("Love to you is just a game.")
     print("You achieved " + str(num) + " points.")
 
+def describe_friend(friend):
+    if friend == '50 Cent':
+        print("He be getting rich or die trying.")
+    elif friend == 'Dr. Dre':
+        print("He be needing California love.")
+    elif friend == 'Rihanna':
+        print("She be needing an umbrella.")
+    else:
+        print("They be cool.")
+
+def display_friend(friend):
+    print(friend + " can be described as follows: ")
+    describe_friend(friend)
+
+def q6():
+    friend = input("Enter a friend: ")
+    add = input("Describe or Display: ")
+    if add == 'Describe':
+        describe_friend(friend)
+    elif add == 'Display':
+        display_friend(friend)
+    else:
+        print("Invalid command.")
+
+
+
 q1()
 print("\n")
 q2()
@@ -59,3 +85,4 @@ q4("monster", "bed")
 q4("Doctor", "Hospital")
 q4("Stranger", "Street")
 q5()
+q6()
--- Week 1/playground.py
+++ Week 1/playground.py
@@ -1,19 +1,26 @@
-def q5():
-    num = 0
-    r = 0
-    print("Let's find out how you sleep...")
-    while r != 4:
-        print("...Baby, how do you sleep when you lie to me?")
-        ans = input()
-        r = r + 1
-        if ans == 'Very well':
-            num = num - 10
-            print("I'm hopin' that my love will keep you up tonight.")
-        elif ans == 'Poorly':
-            num = num + 20
-            print("All that fear and all that pressure.")
-        else:
-            print("Love to you is just a game.")
-    print("You achieved " + str(num) + " points.")
+def describe_friend(friend):
+    if friend == '50 Cent':
+        print("He be getting rich or die trying.")
+    elif friend == 'Dr. Dre':
+        print("He be needing California love.")
+    elif friend == 'Rihanna':
+        print("She be needing an umbrella.")
+    else:
+        print("They be cool.")
 
-q5()
+def display_friend(friend):
+    print(friend + " can be described as follows: ")
+    describe_friend(friend)
+
+def q6():
+    friend = input("Enter a friend: ")
+    add = input("Describe or Display: ")
+    if add == 'Describe':
+        describe_friend(friend)
+    elif add == 'Display':
+        display_friend(friend)
+    else:
+        print("Invalid command.")
+
+
+q6()
