d4b89d2e8de5eb2ea9ca2ba2280d8a2cf757d117
cwilson98 <78536301+cwilson98@users.noreply.github.com>
2021-03-04 21:42:08 -0500

    Added classes to represent clothing.

--- Week 1/human.py
+++ Week 1/human.py
@@ -44,21 +44,8 @@ class Human:
         else:
             return False
 
+    def dress(self, clothing: Clothing) -> None:
+        self.clothing.add(clothing)
 
-
-    def test_eat(self) -> None:
-        # energy is full and try to eat
-        human_prins = Human("Prins")
-        self.assertEqual(human_prins.eat(20), 20, "Excess should be 20.")
-
-        # energy is below 100 and eat more than required
-        human_prins = Human("Prins", energy=90)
-        self.assertEqual(human_prins.eat(20), 10, "Excess should 20.")
-
-        # energy is below 100 and eat exactly what is required
-        human_prins = Human("Prins", energy=80)
-        self.assertEqual(human_prins.eat(20), 0, "Excess should be 0.")
-
-        # energy is below 100 and eat less than required
-        human_prins = Human("Prins", energy=70)
-        self.assertEqual(human_prins.eat(20), -10, "Excess should be -10.")
+    def undress(self, clothing: Clothing) -> None:
+        self.clothing.remove(clothing)
