17b617d9e5c7636f66c0073490abc973bc20b6d9
cwilson98 <78536301+cwilson98@users.noreply.github.com>
2021-03-20 22:00:23 -0400

    Created a basic human class.

---
+++ Week 1/Human.py
@@ -0,0 +1,45 @@
+class Human:
+
+    MAX_ENERGY = 100
+    move_energy = 10
+    reproduce_energy = 20
+
+    def __init__(self, name:str, age:int=0, energy:int=100) -> None:
+        self.name = name
+        self.age = age
+        self.energy = energy
+
+    def __repr__(self) -> str:
+        return f'human(name={self.name}, age={self.age}, energy={self.energy})'
+
+    def __str__(self) -> str:
+        return f'{self.name} is {self.age} years old and has {self.energy} energy'
+
+    def grow(self) -> None:
+        self.age = self.age + 1
+
+    def eat(self, amount: int) -> int:
+        new_energy = self.energy + amount
+        if new_energy > Human.MAX_ENERGY:
+            self.energy = Human.MAX_ENERGY
+            return new_energy - Human.MAX_ENERGY
+        else:
+            self.energy = new_energy
+            return self.energy - Human.MAX_ENERGY
+
+    def reproduce(self) -> bool:
+        new_energy = self.energy - Human.reproduce_energy
+        if new_energy >= Human.reproduce_energy:
+            self.energy = new_energy - Human.reproduce_energy
+            return True
+        else:
+            return False
+
+    def move(self, distance: int) -> bool:
+        new_energy = self.energy - distance
+        if new_energy >= Human.move_energy:
+            self.energy = new_energy
+            return True
+        else:
+            return False
+
--- Week 1/clothing_size.py
+++
@@ -1,8 +0,0 @@
-from enum import Enum
-
-class ClothingSize(Enum):
-    X_SMALL = "extra small"
-    SMALL = "small"
-    MEDIUM = "medium"
-    LARGE = "large"
-    X_LARGE = "extra large"
